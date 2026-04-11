0da51c6da9704bf94dbaeaa89dd75fd83b8136bb
martineekfr <martinek.musil1@gmail.com>
2026-04-11 12:58:48 +0200

    Add FinanceService

--- store_simulator/services/FinanceService.cs
+++ store_simulator/services/FinanceService.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using store_simulator.models;
+
+namespace store_simulator.services;
+
+public class FinanceService
+{
+    private readonly Store _store;
+
+    public FinanceService(Store store)
+    {
+        _store = store;
+    }
+
+    public void ProcessDay(int revenue)
+    {
+        _store.AddRevenue(revenue);
+    }
+}
