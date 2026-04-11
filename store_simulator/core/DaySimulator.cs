3bdcc8eac5284931ab07b6db283a8c1cc6d67098
martineekfr <martinek.musil1@gmail.com>
2026-04-11 12:55:57 +0200

    Add DaySimulator core logic

--- store_simulator/core/DaySimulator.cs
+++ store_simulator/core/DaySimulator.cs
@@ -1,6 +1,32 @@
-﻿namespace store_simulator.core;
+﻿using System;
+using store_simulator.models;
+using store_simulator.services;
+
+namespace store_simulator.core;
 
 public class DaySimulator
 {
-    
+    private readonly Store _store;
+    private readonly CustomerService _customerService;
+    private readonly FinanceService _financeService;
+
+    public DaySimulator(Store store, CustomerService cs, FinanceService fs)
+    {
+        _store = store;
+        _customerService = cs;
+        _financeService = fs;
+    }
+
+    public void RunDay()
+    {
+        var customers = _customerService.GenerateCustomers();
+        var total = 0;
+
+        foreach (var c in customers)
+            total += c.Buy(_store);
+
+        _financeService.ProcessDay(total);
+
+        Console.WriteLine($"Zisk dne: {total}");
+    }
 }
