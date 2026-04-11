dcc62c8623106fbd7a13dbb372caf866e045d6d5
martineekfr <martinek.musil1@gmail.com>
2026-04-11 11:47:33 +0200

    Initial commit

---
+++ store_simulator/Program.cs
@@ -0,0 +1,19 @@
+﻿using store_simulator.core;
+using store_simulator.models;
+using store_simulator.services;
+
+namespace store_simulator;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var store = new Store(1000);
+        var cs = new CustomerService();
+        var fs = new FinanceService(store);
+        var inv = new InventoryService(store);
+
+        var game = new Game(store, cs, fs, inv);
+        game.Run();
+    }
+}
---
+++ store_simulator/core/Game.cs
@@ -0,0 +1,6 @@
+﻿namespace ConsoleApp1.core;
+
+public class Game
+{
+    
+}
